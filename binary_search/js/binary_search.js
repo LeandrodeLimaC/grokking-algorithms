89466998e75c340f676dc2aed8e53c71e0dad4eb
LeandrodeLimaC <leandrolima7472@gmail.com>
2023-11-11 04:32:49 -0300

    add javascript binary search code

---
+++ binary_search/js/binary_search.js
@@ -0,0 +1,26 @@
+function binarySearch(list, item) {
+  let lowIndex = 0;
+  let highIndex = list.length - 1;
+
+  while (lowIndex <= highIndex) {
+    let middle = Math.floor((lowIndex + highIndex) / 2)
+    let guess = list[middle]
+
+    if(guess === item) {
+      return middle
+    }
+    else if (guess > item) {
+      highIndex = --middle
+    }
+    else {
+      lowIndex = ++middle
+    }
+  }
+
+  return null
+}
+
+let arr = ['abc', 'bo', 'c', 'd', 'e']
+console.log(binarySearch(arr, 'bo'))
+
+
